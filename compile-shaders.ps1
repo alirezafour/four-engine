78919b5dbeb51d21c779ddaa21f74271eca0ba00
alirezafour <alirezafour@gmail.com>
2024-03-04 21:21:47 +0300

    update shader compiler script
    - now it create shader folder if not exist and call gslc from installed vulkan directory

--- compile-shaders.ps1
+++ compile-shaders.ps1
@@ -1,2 +1,5 @@
-C:\Dev\VulkanSDK\1.3.250.1\Bin\glslc.exe four-engine\src\renderer\shaders\simpleShader.vert -o four-engine\src\renderer\shaders\simpleShader.vert.spv
-C:\Dev\VulkanSDK\1.3.250.1\Bin\glslc.exe four-engine\src\renderer\shaders\simpleShader.frag -o four-engine\src\renderer\shaders\simpleShader.frag.spv
+if (-not (Test-Path -Path "shaders" -PathType Container)) {
+  New-Item -Path "shaders" -ItemType "directory"
+}
+& "${env:VULKAN_SDK}\Bin\glslc.exe" four-engine\src\renderer\shaders\simpleShader.vert -o shaders\simpleShader.vert.spv
+& "${env:VULKAN_SDK}\Bin\glslc.exe" four-engine\src\renderer\shaders\simpleShader.frag -o shaders\simpleShader.frag.spv
